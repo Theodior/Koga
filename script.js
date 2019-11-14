8f56c44d6215fd35800717de75a22ed50fc02892
Esauu <47736561+Esauu@users.noreply.github.com>
2019-11-14 10:59:10 +0100

    loading

--- script.js
+++ script.js
@@ -1,115 +1,123 @@
-        let cykler = [];
-        const url = "https://esauka.dk/koga/wordpress/wp-json/wp/v2/koga?per_page=100";
-        let filter = "Alle";
+let cykler = [];
+const url = "https://esauka.dk/koga/wordpress/wp-json/wp/v2/koga?per_page=100";
+let filter = "Alle";
 
-        const liste = document.querySelector(".liste");
+const liste = document.querySelector(".liste");
 
-        document.addEventListener("DOMContentLoaded", start);
+document.addEventListener("DOMContentLoaded", start);
 
-        function start() {
-            //            hentNav();
-            hentFooter();
-            hentJson();
-            animer();
-            addEventListenersToButtons();
+function start() {
+    //            hentNav();
+    hentFooter();
+    hentJson();
+    animer();
+    addEventListenersToButtons();
+    setTimeout(showPage, 2000);
+}
+
+//        async function hentNav() {
+//            const response = await fetch("inc/nav.html");
+//            const inclusion = await response.text();
+//            document.querySelector("nav").innerHTML = inclusion;
+//            console.log(inclusion);
+//        }
+
+async function hentFooter() {
+    const response = await fetch("inc/footer.html");
+    const inclusion = await response.text();
+    document.querySelector("footer").innerHTML = inclusion;
+    console.log(inclusion);
+}
+
+function showPage() {
+    console.log("showPage")
+    document.querySelector("#hjul_container").style.display = "none";
+    document.querySelector("#body_container").style.display = "block";
+
+}
+
+async function hentJson() {
+    const response = await fetch(url);
+    cykler = await response.json();
+    console.log(cykler);
+    vis();
+}
+
+
+function vis() {
+    const skabelon = document.querySelector("template");
+    const liste = document.querySelector(".liste");
+
+    liste.innerHTML = "";
+
+    cykler.forEach((cykel) => {
+        if (filter == "Alle" || filter == cykel.kategori) {
+            const klon = skabelon.cloneNode(true).content;
+            klon.querySelector(".title").textContent = cykel.navn;
+
+            klon.querySelector("img").src = cykel.billede.guid;
+            klon.querySelector("img").alt = "billede af" + cykel.title.rendered;
+            klon.querySelector(".beskrivelse").innerHTML = cykel.kort_tekst;
+            liste.appendChild(klon);
+            liste.lastElementChild.addEventListener("click", () => {
+                location.href = `cykelinfo.html?navn=${cykel.navn}`
+            });
         }
-
-        //        async function hentNav() {
-        //            const response = await fetch("inc/nav.html");
-        //            const inclusion = await response.text();
-        //            document.querySelector("nav").innerHTML = inclusion;
-        //            console.log(inclusion);
-        //        }
-
-        async function hentFooter() {
-            const response = await fetch("inc/footer.html");
-            const inclusion = await response.text();
-            document.querySelector("footer").innerHTML = inclusion;
-            console.log(inclusion);
-        }
-
-        async function hentJson() {
-            const response = await fetch(url);
-            cykler = await response.json();
-            console.log(cykler);
-            vis();
-        }
-
-
-        function vis() {
-            const skabelon = document.querySelector("template");
-            const liste = document.querySelector(".liste");
-
-            liste.innerHTML = "";
-
-            cykler.forEach((cykel) => {
-                if (filter == "Alle" || filter == cykel.kategori) {
-                    const klon = skabelon.cloneNode(true).content;
-                    klon.querySelector(".title").textContent = cykel.navn;
-
-                    klon.querySelector("img").src = cykel.billede.guid;
-                    klon.querySelector("img").alt = "billede af" + cykel.title.rendered;
-                    klon.querySelector(".beskrivelse").innerHTML = cykel.kort_tekst;
-                    liste.appendChild(klon);
-                    liste.lastElementChild.addEventListener("click", () => {
-                        location.href = `cykelinfo.html?navn=${cykel.navn}`
-                    });
-                }
-            })
+    })
 
 
 
-        }
+}
 
-        function visSingle(bike) {
-            document.querySelector("#single").style.display = "block";
-            document.querySelector("#single .luk").addEventListener("click", lukSingle);
-            document.querySelector(".beskrivelse h2").textContent = bike.navn;
-            document.querySelector(".beskrivelse .billede").src = bike.billede.guid;
-            document.querySelector(".beskrivelse .billede").alt = "billede af" + bike.navn;
-            document.querySelector(".beskrivelse .pris").textContent = bike.lang_tekst;
-        }
+function visSingle(bike) {
+    document.querySelector("#single").style.display = "block";
+    document.querySelector("#single .luk").addEventListener("click", lukSingle);
+    document.querySelector(".beskrivelse h2").textContent = bike.navn;
+    document.querySelector(".beskrivelse .billede").src = bike.billede.guid;
+    document.querySelector(".beskrivelse .billede").alt = "billede af" + bike.navn;
+    document.querySelector(".beskrivelse .pris").textContent = bike.lang_tekst;
+}
 
-        function lukSingle() {
-            document.querySelector("#single").style.display = "none";
-        }
-
-
-        function animer() {
-            document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-        }
+function lukSingle() {
+    document.querySelector("#single").style.display = "none";
+}
 
 
-        function toggleMenu() {
-            console.log("toggleMenu");
+function animer() {
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
-            document.querySelector("#menu").classList.toggle("hidden");
+}
 
-            let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-            if (erSkjult == true) {
-                document.querySelector("#menuknap").textContent = "☰";
-                document.querySelector("#menuknap").classList.remove("whiten");
 
-            } else {
-                document.querySelector("#menuknap").textContent = "✕";
-                document.querySelector("#menuknap").classList.add("whiten");
+function toggleMenu() {
+    console.log("toggleMenu");
 
-            }
-        }
+    document.querySelector("#menu").classList.toggle("hidden");
 
-        function addEventListenersToButtons() {
-            document.querySelectorAll(".filter").forEach(elm => {
-                elm.addEventListener("click", filtrering);
-            })
-        }
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+        document.querySelector("#menuknap").classList.remove("whiten");
 
-        function filtrering() {
-            filter = this.dataset.kategori;
-            document.querySelector("h1").textContent = this.textContent;
-            document.querySelectorAll(".cykel_filter").forEach(elm => {
-                elm.classList.remove("valgt");
-            })
-            this.classList.add("valgt");
-            vis();
-        }
+    } else {
+        document.querySelector("#menuknap").textContent = "✕";
+        document.querySelector("#menuknap").classList.add("whiten");
+
+    }
+}
+
+function addEventListenersToButtons() {
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.addEventListener("click", filtrering);
+    })
+}
+
+function filtrering() {
+    filter = this.dataset.kategori;
+    document.querySelector("h1").textContent = this.textContent;
+    document.querySelectorAll(".cykel_filter").forEach(elm => {
+        elm.classList.remove("valgt");
+    })
+    this.classList.add("valgt");
+    vis();
+}
