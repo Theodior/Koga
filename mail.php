595f74ed720591f581e237517b7b2c876e01666e
Esauu <47736561+Esauu@users.noreply.github.com>
2019-11-19 11:41:07 +0100

    php

--- mail.php
+++ mail.php
@@ -1,11 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $name = $_POST ['name'];
 $email = $_POST ['email'];
 $message = $_POST ['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "info@esauka.dk";
-$subject = "Contact Form";
-$mailheader = "From: $name \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die ("Error!");
-echo "MANGE TAK SKAL HAVE MIN VEN";
+$recipient = "esau@esauka.dk";
+$subject = "mail fra kunden";
+//$mailheader = "From: $name \r\n";
+mail($recipient, $subject, $formcontent) or die ("Error!");
+echo "Jeg vender tilbage så hurtigt som muligt";
 ?>
